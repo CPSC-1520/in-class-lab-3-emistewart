cf3edb907581a912a0c5b3ca286cda3fb02127da
Emily Stewart <estewart13@nait.ca>
2024-10-02 22:36:55 -0600

    moving foward from current confusion

--- js/main.js
+++ js/main.js
@@ -2,10 +2,19 @@
 
 const resourceShow=document.querySelector(".javascript-resources");
 
-
 const div = document.createElement("div");
 const btn=document.querySelector('.btn');
 btn.addEventListener('click', (event)=>{
     event.preventDefault();
     div.classList.remove("d-none")
-})
+});
+
+resourceShow.addEventListener("mouseover", (event)=>{
+    console.log(event.target)
+    event.target.classList.add("fw-bold")
+});
+
+resourceShow.addEventListener("mouseout", (event)=>{
+    console.log(event.target)
+    event.target.classList.remove("fw-bold")
+});
