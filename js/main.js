607ce91b418a906825b3dabd63b96eee8537fad3
Emily Stewart <estewart13@nait.ca>
2024-10-02 21:49:27 -0600

    trying first exercise

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // Enter your code below.
+
+const resourceShow=document.querySelector(".javascript-resources");
+
+const btn=document.querySelector('.btn');
+btn.addEventListener('click', (event)=>{
+    event.preventDefault();
+    div.classList.remove('d-none');
+})
