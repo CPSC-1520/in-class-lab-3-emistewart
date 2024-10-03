ce0949d303cf787a95945fb3321a6c80414d7b43
Emily Stewart <estewart13@nait.ca>
2024-10-02 22:16:22 -0600

    nothing is working

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 // Enter your code below.
 
-const resourceShow=document.querySelector(".javascript-resources");
+//const resourceShow=document.querySelector(".javascript-resources");
 
+function resourceShow () {
+    let resources = document.querySelector(".javascript-resources");
+    resources.getElementsByClassName('d-none');
+    resources.classList.remove('d-none');
+}
+
+const div = document.createElement("div");
 const btn=document.querySelector('.btn');
 btn.addEventListener('click', (event)=>{
     event.preventDefault();
-    div.classList.remove('d-none');
+    div.classList.remove("d-none")
 })
